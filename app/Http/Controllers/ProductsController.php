577d8ce22270c5d200cfc260987baa5cc4fe3e93
unknown <caloy0714@gmail.com>
2022-09-30 14:19:04 +0800

    all products

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+require "vendor/autoload.php";
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use App\Models\Products;
 
 class ProductsController extends Controller
 {
-    //
+    public function getAllProducts(){
+        $client = new Client([
+                'base_uri' => 'http://127.0.0.1:8000/'
+        ]);
+        $response = $client->get('http://127.0.0.1:8000/products');
+        $code = $response->getStatusCode();
+        $body = $response->getBody();
+        $products=json_decode($body)->products;
+        var_dump($products);
+        }
 }
+
